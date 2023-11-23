7f2fa5fe794bd869dff1bf91a19de4e3aa77458a
NgocLinh3110 <linhvo31102002@gmail.com>
2023-11-23 14:32:12 +0700

    add 3 screen

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/Home';
+import More from './src/More';
 import PhatHien from './src/PhatHien';
-const Stack = createNativeStackNavigator();
+import Favorite from './src/Favorite';
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator options={{headerShown: false}}>
-        <Stack.Screen name='PhatHien' component={PhatHien} options={{headerShown: false}}/>
-      </Stack.Navigator>
+      <Tab.Navigator options={{headerShown: false}}>
+        <Tab.Screen name='Home' component={Home} options={{headerShown: false}}/>
+        <Tab.Screen name ='PhatHien' component={PhatHien} options={{headerShown: false}}/>
+        <Tab.Screen name='Favorite' component={Favorite} options={{headerShown: false}}/>
+        <Tab.Screen name='More' component={More} options={{headerShown: false}}/>
+
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Pressable, Animated, FlatList} from 'react-native';
+import React, { useState } from 'react';
+import  { useRef } from 'react';
+
+function Home({navigation}) {
+  return (
+  <View style={styles.container}>
+    <View>
+      <Text style={styles.text1}>U-Dictionary</Text>
+    </View>
+    <Pressable style={styles.translate}>
+
+    </Pressable>
+    <View style={styles.options}>
+
+    </View>
+  </View>
+);
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },  
+  text1:{
+    width:'180px',
+    height: '45px',
+    fontFamily:'SVN-Gilroy',
+    fontSize:30,
+    fontWeight:700,
+    color:'#fff',
+    textAlign: 'left',
+    marginBottom: '20px'
+  },
+  translate:{
+    width: '370px',
+    height: '150px',
+    borderWidth: '2px',
+    borderColor: '#81BEE0',
+    borderRadius: '20px'
+  },
+  options:{
+
+  },
+});
+export default Home;
