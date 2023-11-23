a7cc70893189452698c291e09709bd8f6410c402
NgocLinh3110 <85429214+NgocLinh3110@users.noreply.github.com>
2023-11-23 16:02:14 +0700

    UI home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Pressable, Animated, FlatList} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Pressable, Animated, FlatList} from 'react-native';
 import React, { useState } from 'react';
 import  { useRef } from 'react';
 
@@ -9,8 +9,89 @@ function Home({navigation}) {
       <Text style={styles.text1}>U-Dictionary</Text>
     </View>
     <Pressable style={styles.translate}>
-
+        <Pressable>
+            <Pressable style={{flex:1, flexDirection:'row', alignContent: 'center'}}>
+                <Text style={styles.input}>Nhập ký tự</Text>
+                <Image
+                    source={require('../image/audio.png')}
+                    style={{ width: 17, height: 17}}
+                />
+            </Pressable>
+        </Pressable>
     </Pressable>
+    <View style={{flex:1, flexDirection:'row', alignContent: 'center'}}>
+        <Pressable style={{flex:1, flexDirection:'row'}}>
+            <Pressable style={styles.uDiction}>
+                <Image
+                    source={require('../image/letter-u.png')}
+                    style={{ width: 60, height: 60, marginTop: 30, marginLeft: 50}}
+                />
+                <Text style={{ 
+                        width: 50, 
+                        height: 16, 
+                        marginLeft: 55, 
+                        textAlign:'center',
+                        fontFamily: 'SVN-Gilroy',
+                        fontSize:13,
+                        fontWeight:700,
+                        color: 'white'
+                    }}>Màu</Text>
+            </Pressable>
+            <Pressable style={{flex:1,flexDirection:'column'}}>  
+                <Pressable style={styles.subUDiction}>
+                    <Image
+                        source={require('../image/camera.png')}
+                        style={{ width: 25, height: 25, marginTop: 10, marginLeft: 5}}
+                    />
+                    <Text style={{ 
+                            width: 100, 
+                            height: 16, 
+                            marginTop: 10,
+                            marginLeft: 10, 
+                            textAlign:'left',
+                            fontFamily: 'SVN-Gilroy',
+                            fontSize:13,
+                            fontWeight:700,
+                            color: 'white'
+                        }}>Camera</Text>
+                </Pressable>
+                <Pressable style={styles.subUDiction}>
+                    <Image
+                        source={require('../image/translator.png')}
+                        style={{ width: 25, height: 25, marginTop: 10, marginLeft: 5}}
+                    />
+                    <Text style={{ 
+                            width: 100, 
+                            height: 16, 
+                            marginTop: 10,
+                            marginLeft: 10, 
+                            textAlign:'left',
+                            fontFamily: 'SVN-Gilroy',
+                            fontSize:13,
+                            fontWeight:700,
+                            color: 'white'
+                        }}>Đoạn thoại</Text>
+                </Pressable>
+                <Pressable style={styles.subUDiction}>
+                    <Image
+                        source={require('../image/notes.png')}
+                        style={{ width: 25, height: 25, marginTop: 10, marginLeft: 5}}
+                    />
+                    <Text style={{ 
+                            width: 100, 
+                            height: 16, 
+                            marginTop: 10,
+                            marginLeft: 10, 
+                            textAlign:'left',
+                            fontFamily: 'SVN-Gilroy',
+                            fontSize:13,
+                            fontWeight:700,
+                            color: 'white'
+                        }}>Ngữ pháp</Text>
+                </Pressable>
+            </Pressable>
+        </Pressable>
+    </View>
     <View style={styles.options}>
 
     </View>
@@ -19,6 +100,7 @@ function Home({navigation}) {
 }
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
@@ -38,10 +120,38 @@ const styles = StyleSheet.create({
     height: '150px',
     borderWidth: '2px',
     borderColor: '#81BEE0',
-    borderRadius: '20px'
+    borderRadius: '20px',
+    marginBottom: 30,
+    backgroundColor: '#2D2D2D',
   },
-  options:{
-
+  input:{
+    width:'260px',
+    height: '55px',
+    color: '#13597E',
+    fontFamily: 'SVN-Gilroy',
+    fontSize:30,
+    fontWeight:300,
+    textAlign: 'left',
+    marginTop: '60px',
+    marginLeft: '16px'
+  },
+  uDiction:{
+    width: 170,
+    height: 170,
+    borderRadius: 10,
+    backgroundColor: '#2D2D2D',
+    justifyContent: 'center',
+    marginRight: 10,
+    flexDirection: 'column'
+  },
+  subUDiction:{
+    width: 170,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: '#2D2D2D',
+    justifyContent: 'center',
+    marginBottom: 10,
+    flexDirection: 'row'
   },
 });
 export default Home;
